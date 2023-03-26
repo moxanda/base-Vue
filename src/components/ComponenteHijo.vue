2770f0644df8a55b8aa0b4587eb747827f278a48
Marcel Oxandabaratz <70418861+moxanda@users.noreply.github.com>
2023-03-26 00:13:52 +0000

    Initial commit

---
+++ src/components/ComponenteHijo.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1> ComponenteHijo {{mensaje}} <h1> {{param1}} </h1> </h1>
+</template>
+<script setup lang='ts'>
+  // import { ref } from 'vue'
+  // let mensaje = ref('Prueba')
+  let mensaje = 'PruebA'
+  defineProps({ param1: String, param2: String })
+/*  props: {
+  menmen: String
+}
+*/
+</script>
